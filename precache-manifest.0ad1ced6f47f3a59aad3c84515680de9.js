8b51e892acc4af5b57773768ee1ca0faf2d3ac4f
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2020-02-10 20:55:17 +0000

    Updates

---
+++ precache-manifest.0ad1ced6f47f3a59aad3c84515680de9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a07a826bf9f78657972cb7be976ca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b55c7fc62bb5ae885f93",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "86a8c3df006c6db650af",
+    "url": "/static/js/2.c74080fe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c74080fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b55c7fc62bb5ae885f93",
+    "url": "/static/js/main.1379b374.chunk.js"
+  },
+  {
+    "revision": "04e80014664f5835afd5",
+    "url": "/static/js/runtime-main.d052d0e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
