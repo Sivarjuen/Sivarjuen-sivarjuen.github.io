19c7e349e74c2b07673eef650a78d7885613c4ba
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2022-03-05 16:53:46 +0000

    Add sample blogs and migrate to typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,13 @@ import Link from "next/link";
 const name = "Sivarjuen Ravichandran";
 export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -69,5 +75,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  );
+  )
 }
